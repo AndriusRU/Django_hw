3c6a549a63527639c8e60890e1bd7044e0593fa2
Ячный Андрей <andrey.yachniy@ifellow.ru>
2022-11-20 15:14:23 +0700

    Update Task Smart-Home Django

--- 3.1-drf-intro/smart_home/measurement/admin.py
+++ 3.1-drf-intro/smart_home/measurement/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Measurement
 # Register your models here.
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    pass
--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
+def image_path(instance, filename):
+    return '/'.join(['sensorImages', f'sensor_id_{instance.sensor.id}', filename])
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to=image_path, null=True, blank=True)
