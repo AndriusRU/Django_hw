2abafa0d82bb9c1e255a0eafcf2a3554dda32c4f
Ячный Андрей <andrey.yachniy@ifellow.ru>
2022-11-29 09:35:40 +0700

    Change Task Stocks_Products

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,8 +1,8 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
-from .filter import FilterStock
 
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
@@ -17,6 +17,6 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['products']
-    filterset_class = FilterStock
+    search_fields = ['products__title', 'products__description']
